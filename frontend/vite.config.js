130e7918fd564e1f8af8e38a8df8d62cce36ab8a
Tushar <tusharsharma5347@gmail.com>
2025-08-20 16:39:31 +0530

    Deployment changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://clinic-booking-fz75.onrender.com',
         changeOrigin: true,
         secure: false,
       },
